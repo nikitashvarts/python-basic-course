638b9017e0e88d1d8a3be87e2ad1be29e6674acf
Konstantin Moskalenko <kkmoskalenko@icloud.com>
2023-11-30 18:34:58 +0700

    Implement error handling

--- projects/task_manager/app.py
+++ projects/task_manager/app.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import sqlite3
 from flask import Flask
 from routes import tasks_bp
 
@@ -8,7 +10,16 @@ app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'tasks.sqlite')
 )
 
-os.makedirs(app.instance_path, exist_ok=True)
+
+@app.errorhandler(sqlite3.Error)
+def handle_error(err):
+    return f'Database operation failed: {err}', 500
+
+
+try:
+    os.makedirs(app.instance_path, exist_ok=True)
+except OSError as e:
+    sys.exit(f'Failed to create the instance directory at {e.filename}')
 
 if __name__ == '__main__':
     app.run()
